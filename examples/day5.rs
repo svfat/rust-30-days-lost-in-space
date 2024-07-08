d664d290c9d887cffc01532739be12fbf8edb102
Stan Fateev <stanfateev@gmail.com>
2024-07-08 16:23:31 +0100

    code: Day 5

--- examples/day5.rs
+++ examples/day5.rs
@@ -6,6 +6,17 @@ use panic_halt as _;
 const MIN_DELAY: u16 = 50;
 const MAX_DELAY: u16 = 500;
 
+fn map(x: u16, in_min: u16, in_max: u16, out_min: u16, out_max: u16) -> u16 {
+    if in_max == in_min {
+        return out_min
+    };
+    let in_range = (in_max - in_min) as u64;
+    let out_range = (out_max - out_min) as u64;
+    let scaled_value = ((x - in_min) as u64 * out_range) / in_range;
+    (scaled_value as u16) + out_min
+}
+
+
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
@@ -17,8 +28,25 @@ fn main() -> ! {
     let photoresistor = pins.a0.into_analog_input(&mut adc);
     let mut serial = arduino_hal::default_serial!(dp, pins, 9600);
 
+    let mut darkest_light: u16  = 1024;
+    let mut brightest_light: u16 = 0;
+
     loop {
-        let light_value = photoresistor.analog_read(&mut adc);
-        ufmt::uwriteln!(&mut serial, "Light value: {}", light_value);
+        let light_value: u16 = photoresistor.analog_read(&mut adc);
+        _ = ufmt::uwriteln!(&mut serial, "Light value: {}\r\n", light_value);
+
+        if light_value < darkest_light {
+            darkest_light = light_value;
+        };
+        if light_value > brightest_light {
+            brightest_light = light_value;
+        };
+
+        let delay_value = map(light_value, darkest_light, brightest_light, MIN_DELAY, MAX_DELAY);
+
+        _ = ufmt::uwriteln!(&mut serial, "Delay value: {}\r\n", delay_value);
+
+        led.toggle();
+        arduino_hal::delay_ms(delay_value);
     }
 }
